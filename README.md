53684e197b6ed74040f7fffbd66ba391fd639d8b
jondough <ewhimsical@gmail.com>
2023-10-17 22:00:00 -0700

    added CLI options, support for crafting rotation

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # apex-live-info
-Script that gets the current map rotation in Apex Legends.
-
-Will probably add command-line arguments and/or support for continual updates soon.
+Script that grabs live info from [Apex Legends Status](https://apexlegendsstatus.com/).
+Currently supports crafting and map rotations.
 
 May be repurposed into a Discord bot or something later, TBD.
 
@@ -11,4 +10,17 @@ First, register for an API key [here](https://apexlegendsapi.com/).
 
 Rename `config.cfg` to `api_config.cfg` and paste your API key into the `CLIENT_ID` field.
 
-Run `main.py` to get a list of maps for each game mode printed to the terminal.
+Run `python main.py` with the appropriate arguments and options to get live info.
+
+**Arguments**
+
+`-r <R>` or `--request <REQUEST>` : Specifies the desired live info request with a desired option.
+
+Request options:
+
+`crafting` : Gets current crafting rotation.
+
+`map` : Gets current map rotation for BR Pubs, BR Ranked, and Mixtape (Rotating LTM).
+
+### Example Argument
+`python main.py -r map` Gets the current map rotation (BR Pubs, BR Ranked, and rotating LTM).
--- main.py
+++ main.py
@@ -1,20 +1,42 @@
+import argparse
 import configparser
 import requests
 from requests.exceptions import HTTPError
 
+# get API key
 config = configparser.ConfigParser()
 config.read('api_config.cfg')
 auth = '?auth=' + config['Credentials']['CLIENT_ID']
 BASE_URL = 'https://api.mozambiquehe.re/'
 
 def main():
-    req = 'maprotation'
-    param = 'version=2'
-    json = make_request(req, param)
-    maps = get_current_maps(json)
-    print("Current map rotation:")
-    for mode in maps:
-        print(mode + ": " + maps[mode])
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--request', type=str,
+                        help='Specify the request type (crafting, map, or store)', required=True)
+    args = parser.parse_args()
+
+    # check for valid args
+    if args.request:
+        param = ''
+        if 'crafting'.casefold() == args.request.casefold():
+            req = 'crafting'
+            json = make_request(req, param)
+            items = get_current_crafting(json)
+            print('Current Crafting Rotation:')
+            print_results(items, ' | ')
+        elif 'map'.casefold() == args.request.casefold():
+            req = 'maprotation'
+            param = 'version=2'
+            json = make_request(req, param)
+            maps = get_current_maps(json)
+            print('Current Map Rotation:')
+            print_results(maps, ': ')
+        elif 'store'.casefold() == args.request.casefold():
+            req = 'store'
+            # TODO
+        else:
+            raise ValueError('Invalid request type. Try crafting, map, or store.')
 
 
 def make_request(req, param):
@@ -31,12 +53,51 @@ def make_request(req, param):
         raise SystemExit
 
 
+def get_current_crafting(json):
+    items = {}
+    for j in json:
+        for b in j['bundleContent']:
+            item = capitalize_string(b['itemType']['name'].replace('_', ' '))
+            rarity = b['itemType']['rarity']
+            cost = b['cost']
+            if item not in items:
+                items[item] = 'Rarity: ' + rarity + ' | Cost: ' + str(cost)
+    return items
+
+
 def get_current_maps(json):
     res = {}
     res['BR Pubs'] = json['battle_royale']['current']['map']
     res['BR Ranked'] = json['ranked']['current']['map']
     res['LTM - ' + json['ltm']['current']['eventName']] = json['ltm']['current']['map']
     return res
+
+
+def get_current_store(json):
+    # TODO
+    return None
+
+
+def print_results(res, spacer):
+    for r in res:
+        print(r + spacer + res[r])
+
+
+# Custom function that capitalizes the first letter of every word.
+# Compared to other similar functions, this one does not alter the capitalization of other letters.
+def capitalize_string(str):
+    words = str.split()
+    cap_words = [capitalize_first_letter(word) for word in words]
+    return ' '.join(cap_words)
+
+
+# Capitalizes first letter of a word, but leaves the rest untouched.
+def capitalize_first_letter(word):
+    if word:
+        return word[:1].upper() + word[1:]
+    else:  # in case input is empty
+        return word
+
 
 if __name__== "__main__":
     main()
